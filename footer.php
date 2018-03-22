bc8546ec34663ea903c28b623eb5e7a18a2d9ae3
Nerijus Anužis <nerijusanz@gmail.com>
2018-03-22 17:46:00 +0200

    remove txt line

--- footer.php
+++ footer.php
@@ -16,24 +16,8 @@
 	<footer id="colophon" class="site-footer container-fluid" role="contentinfo">
 		<div class="site-info">
 
-            <?php
-            if ( has_nav_menu( 'footer' ) ) :
-                wp_nav_menu( array(
-                    'theme_location' => 'footer',
-                    'menu_id' => 'ca-footer-menu',
-                    //'walker' => new CA\Core\WalkerNav(),
-                ) );
-            endif;
-            ?>
-
-			<a href="<?php
-				/* translators: %s: Github repo URL. */
-				echo esc_url( __( 'https://codearchitect.lt', 'codearchitect' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'codearchitect' ), 'codearchitect' ); ?></a>
-			<span class="sep"> | </span>
 			<?php
-				/* translators: %1: Theme name. */
-
-				/* translators: %2: Author name. */
+            
 				printf( esc_html__( 'Theme: %1$s by %2$s %3$s', 'codearchitect' ),
 										'codearchitect',
 										'<a href="http://codearchitect.lt/" rel="designer">Codearchitect</a>' ,
